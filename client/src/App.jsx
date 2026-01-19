b2731e6e4342ff2b9b41684742f7031ab51397ea
SanthoshLSA <santhosh.ananth6@gmail.com>
2026-01-19 23:01:36 +0530

    created folder structure and basic pages, navbar and linked clerk

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom' 
+import ApplyJob from './pages/ApplyJob'
+import Applications from './pages/Applications'
+import Home from './pages/Home'
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/applications' element={<Applications />} />
+        <Route path='/apply-job/:id' element={<ApplyJob />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
